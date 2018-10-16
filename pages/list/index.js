5f76eb68009ea8876eb3ffdd97da2041ccc70d51
张安伦 <anlunz@126.com>
2018-10-16 15:17:05 +0800

    UI修改-榜单

--- pages/list/index.js
+++ pages/list/index.js
@@ -3,11 +3,12 @@ const { getList } = require('../../request/request');
 Page({
   data: {
     sizes: [3, 4, 5],
-    chooseType: 3,
-    list: []
+   // chooseType: 3,
+    list: [],
+    item:3
   },
   onLoad: function () {
-    this.getLists(this.data.chooseType)
+    this.getLists(this.data.item)
   },
   getLists: function (type) {
     getList(type).then(data => {
@@ -18,5 +19,15 @@ Page({
     let chooseType = event.detail.value;
     this.setData({chooseType})
     this.getLists(chooseType)
-  }
+  },
+  changeList:function(event){
+    if(this.data.item ==3 || this.data.item ==4){
+      this.setData({item:this.data.item + 1})
+    }else{
+      this.setData({item:3})
+    }
+    this.getLists(this.data.item)
+
+    console.log('change!!!!',event)
+  },
 })
